f9b4243094327fa985411522b4311ad78c3060b2
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-21 21:53:32 -0300

    fix selecao_screen

--- inovaeuro/lib/screens/Login/selecao_screen.dart
+++ inovaeuro/lib/screens/Login/selecao_screen.dart
@@ -13,33 +13,31 @@ class _SelecaoScreenState extends State<SelecaoScreen> {
     return Scaffold(
       body: SafeArea(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text('Inova Euro',
-            style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold,
-            ),
-            ),
-
-            const SizedBox(height: 40),
-
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+            const Expanded(child: SizedBox()), // espaço acima
+            Column(
+              mainAxisSize: MainAxisSize.min,
               children: [
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.black,
-                    foregroundColor: Colors.white,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 24, vertical: 12
-                    ),
-                  ),
-                  onPressed: () {
-
-                    },
-                    child: const Text('Executivo'),
+                const Text(
+                  'Inova Euro',
+                  style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 40),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.black,
+                        foregroundColor: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 24, vertical: 12),
+                      ),
+                      onPressed: () {},
+                      child: const Text('Executivo'),
                     ),
                     const SizedBox(width: 16),
                     OutlinedButton(
@@ -48,48 +46,48 @@ class _SelecaoScreenState extends State<SelecaoScreen> {
                         shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(20),
                         ),
-                        padding: (const EdgeInsets.symmetric(
-                           horizontal: 24, vertical: 12)
-                        ),
-                        onPressed: () {
-                          
-                        }, 
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 24, vertical: 12),
+                      ),
+                      onPressed: () {},
                       child: const Text(
                         'Empreendedor',
                         style: TextStyle(color: Colors.black),
+                      ),
                     ),
-                  ),  
-                ],
-              ),
-
-              const Spacer(),
-
-              Padding(
-                padding: const EdgeInsets.only(bottom: 16),
-                child: Text.rich(
-                  TextSpan(
-                    text: 'By clicking continue, you agree to our ',
-                    style: const TextStyle(fontSize: 12, color: Colors.grey),
-                    children: [
-                      TextSpan(
-                        text: 'Terms of Service',
-                        style: const TextStyle(
-                          fontSize: 12,
-                          color: Colors.blue,
-                          decoration: TextDecoration.underline),
-                        ),
-                        const TextSpan(text: 'and'),
-                        TextSpan(
-                          text:'Privacy Policy',
-                          style: const TextStyle(
-                            fontSize: 12,
-                          color: Colors.blue,
-                          decoration: TextDecoration.underline),
-                          ),
-                          const TextSpan(text: 'by BlueFarma'),
-                    ],
-                  ),
-                  textAlign: TextAlign.center,
+                  ],
+                ),
+              ],
+            ),
+            const Expanded(child: SizedBox()), // espaço abaixo
+            Padding(
+              padding: const EdgeInsets.only(bottom: 16),
+              child: Text.rich(
+                TextSpan(
+                  text: 'By clicking continue, you agree to our ',
+                  style: const TextStyle(fontSize: 12, color: Colors.grey),
+                  children: [
+                    TextSpan(
+                      text: 'Terms of Service',
+                      style: const TextStyle(
+                        fontSize: 12,
+                        color: Colors.blue,
+                        decoration: TextDecoration.underline,
+                      ),
+                    ),
+                    TextSpan(text: ' and '),
+                    TextSpan(
+                      text: 'Privacy Policy',
+                      style: const TextStyle(
+                        fontSize: 12,
+                        color: Colors.blue,
+                        decoration: TextDecoration.underline,
+                      ),
+                    ),
+                    TextSpan(text: ' by BlueFarma'),
+                  ],
+                ),
+                textAlign: TextAlign.center,
               ),
             ),
           ],
